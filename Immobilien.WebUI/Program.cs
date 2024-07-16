8b3ace95af7a3ae29f6d0553f4c5671f9ae62a50
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-07-16 15:23:35 +0200

    Prepared for mongoDB ImmobilienContext.cs, program.cs, appsettings.json

--- Immobilien.WebUI/Program.cs
+++ Immobilien.WebUI/Program.cs
@@ -1,6 +1,20 @@
+using Immobilien.DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+var mongoDatabase = new MongoClient(builder.Configuration.GetConnectionString("MongoConnection")).GetDatabase(builder.Configuration.GetSection("DatabaseName").Value);
+
+builder.Services.AddDbContext<ImmobilienContext>(option =>
+{
+    option.UseMongoDB(mongoDatabase.Client, mongoDatabase.DatabaseNamespace.DatabaseName);
+});
+
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
