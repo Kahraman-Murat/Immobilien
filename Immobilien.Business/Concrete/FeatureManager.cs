84bf23aa540aba81f914839e05beaeac69d79a19
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-07-16 20:28:01 +0200

    Prepeared Business Layer

---
+++ Immobilien.Business/Concrete/FeatureManager.cs
@@ -0,0 +1,18 @@
+﻿using Immobilien.Business.Abstract;
+using Immobilien.DataAccess.Abstract;
+using Immobilien.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Immobilien.Business.Concrete
+{
+    public class FeatureManager : GenericManager<Feature>, IFeatueService
+    {
+        public FeatureManager(IGenericDal<Feature> genericDal) : base(genericDal)
+        {
+        }
+    }
+}
