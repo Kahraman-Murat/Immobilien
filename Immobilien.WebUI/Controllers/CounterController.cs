5de572fd3235d138eb0898711f4aee613699de50
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-07-17 22:41:12 +0200

    Prepeared CounterColntroller and Dtos files and Mapping

---
+++ Immobilien.WebUI/Controllers/CounterController.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using Immobilien.Business.Abstract;
+using Immobilien.Dto.Dtos.CounterDtos;
+using Immobilien.Entity.Entities;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+
+namespace Immobilien.WebUI.Controllers
+{
+    public class CounterController : Controller
+    {
+
+        private readonly ICounterService _counterService;
+        private readonly IMapper _mapper;
+
+        public CounterController(ICounterService counterService, IMapper mapper)
+        {
+            _counterService = counterService;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var datas = await _counterService.TGetListAsync();
+            var counterList = _mapper.Map<List<ResultCounterDto>>(datas);
+            return View(counterList);
+        }
+
+        public async Task<IActionResult> DeleteCounter(ObjectId id)
+        {
+            await _counterService.TDeleteAsync(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult CreateCounter(ObjectId id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCounter(CreateCounterDto createCounterDto)
+        {
+            var newCounter = _mapper.Map<Counter>(createCounterDto);
+            await _counterService.TCreateAsync(newCounter);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> UpdateCounter(ObjectId id)
+        {
+            var datas = await _counterService.TGetByIdAsync(id);
+            var counter = _mapper.Map<UpdateCounterDto>(datas);
+            return View(counter);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateCounter(UpdateCounterDto updateCounterDto)
+        {
+            var counter = _mapper.Map<Counter>(updateCounterDto);
+            await _counterService.TUpdateAsync(counter);
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Immobilien.WebUI/Mapping/GeneralMapping.cs
+++ Immobilien.WebUI/Mapping/GeneralMapping.cs
@@ -1,6 +1,13 @@
 ﻿using AutoMapper;
 using Immobilien.Dto.Dtos.BannerDtos;
 using Immobilien.Dto.Dtos.ContactDtos;
+using Immobilien.Dto.Dtos.CounterDtos;
+using Immobilien.Dto.Dtos.DealDtos;
+using Immobilien.Dto.Dtos.FeatureDtos;
+using Immobilien.Dto.Dtos.MessageDtos;
+using Immobilien.Dto.Dtos.ProductDtos;
+using Immobilien.Dto.Dtos.QuestDtos;
+using Immobilien.Dto.Dtos.VideoDtos;
 using Immobilien.Entity.Entities;
 
 namespace Immobilien.WebUI.Mapping
@@ -17,6 +24,33 @@ namespace Immobilien.WebUI.Mapping
             CreateMap<UpdateContactDto, Contact>().ReverseMap();
             CreateMap<CreateContactDto, Contact>().ReverseMap();
 
+            CreateMap<ResultCounterDto, Counter>().ReverseMap();
+            CreateMap<UpdateCounterDto, Counter>().ReverseMap();
+            CreateMap<CreateCounterDto, Counter>().ReverseMap();
+
+            CreateMap<ResultDealDto, Deal>().ReverseMap();
+            CreateMap<UpdateDealDto, Deal>().ReverseMap();
+            CreateMap<CreateDealDto, Deal>().ReverseMap();
+
+            CreateMap<ResultFeatureDto, Feature>().ReverseMap();
+            CreateMap<UpdateFeatureDto, Feature>().ReverseMap();
+            CreateMap<CreateFeatureDto, Feature>().ReverseMap();
+
+            CreateMap<ResultMessageDto, Message>().ReverseMap();
+            CreateMap<UpdateMessageDto, Message>().ReverseMap();
+            CreateMap<CreateMessageDto, Message>().ReverseMap();
+
+            CreateMap<ResultProductDto, Product>().ReverseMap();
+            CreateMap<UpdateProductDto, Product>().ReverseMap();
+            CreateMap<CreateProductDto, Product>().ReverseMap();
+
+            CreateMap<ResultQuestDto, Quest>().ReverseMap();
+            CreateMap<UpdateQuestDto, Quest>().ReverseMap();
+            CreateMap<CreateQuestDto, Quest>().ReverseMap();
+
+            CreateMap<ResultVideoDto, Video>().ReverseMap();
+            CreateMap<UpdateVideoDto, Video>().ReverseMap();
+            CreateMap<CreateVideoDto, Video>().ReverseMap();
         }
     }
 }
