e76d04888e40487d241afb4cd0102e6ebc6baffb
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-07-22 00:24:02 +0200

    Prepeared Question Section in UILayout

---
+++ Immobilien.WebUI/ViewComponents/DefaultIndex/_DefaultQuest.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Immobilien.Business.Abstract;
+using Immobilien.Dto.Dtos.QuestDtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Immobilien.WebUI.ViewComponents.DefaultIndex
+{
+    public class _DefaultQuest : ViewComponent
+    {
+        private readonly IQuestService _questService;
+        private readonly IMapper _mapper;
+
+        public _DefaultQuest(IMapper mapper, IQuestService questService)
+        {
+            _mapper = mapper;
+            _questService = questService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var datas = await _questService.TGetListAsync();
+            var questList = _mapper.Map<List<ResultQuestDto>>(datas);
+            return View(questList);
+        }
+    }
+}
