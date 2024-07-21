639edf5cf6baf645acf49a7a68ae94b611221e25
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-07-21 22:24:21 +0200

    Added field Payment in Feature Entity and prepeared UILayaut Component Feature

---
+++ Immobilien.WebUI/ViewComponents/DefaultIndex/_DefaultFeature.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Immobilien.Business.Abstract;
+using Immobilien.Dto.Dtos.FeatureDtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Immobilien.WebUI.ViewComponents.DefaultIndex
+{
+    public class _DefaultFeature : ViewComponent
+    {
+        private readonly IFeatureService _featureService;
+        private readonly IMapper _mapper;
+
+        public _DefaultFeature(IFeatureService featureService, IMapper mapper)
+        {
+            _featureService = featureService;
+            _mapper = mapper;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var datas = await _featureService.TGetListAsync();
+            var featureList = _mapper.Map<List<ResultFeatureDto>>(datas);
+            return View(featureList);
+        }
+    }
+}
