a78de3b3c2a52ecc087e879e858572ca87054c9d
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-07-22 20:28:21 +0200

    Prepeared Product Section in UILayout

---
+++ Immobilien.WebUI/ViewComponents/DefaultIndex/_DefaultProduct.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Immobilien.Business.Abstract;
+using Immobilien.Dto.Dtos.ProductDtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Immobilien.WebUI.ViewComponents.DefaultIndex
+{
+    public class _DefaultProduct : ViewComponent
+    {
+        private readonly IProductService _productService;
+        private readonly IMapper _mapper;
+        public _DefaultProduct(IProductService productService, IMapper mapper)
+        {
+            _productService = productService;
+            _mapper = mapper;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var datas = await _productService.TGetListAsync();
+            var productList = _mapper.Map<List<ResultProductDto>>(datas);
+            return View(productList);
+        }
+    }
+}
