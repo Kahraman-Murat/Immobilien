70f2ba9898a146ff7f4c520aa10a251c4be45768
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-07-22 18:06:30 +0200

    Prepeared Counter Section in UILayout

---
+++ Immobilien.WebUI/ViewComponents/DefaultIndex/_DefaultCounter.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Immobilien.Business.Abstract;
+using Immobilien.Dto.Dtos.CounterDtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Immobilien.WebUI.ViewComponents.DefaultIndex
+{
+    public class _DefaultCounter : ViewComponent
+    {
+        private readonly ICounterService _counterService;
+        private readonly IMapper _mapper;
+
+        public _DefaultCounter(ICounterService counterService, IMapper mapper)
+        {
+            _counterService = counterService;
+            _mapper = mapper;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var datas = await _counterService.TGetListAsync();
+            var counterList = _mapper.Map<List<ResultCounterDto>>(datas);
+            return View(counterList);
+        }
+
+    }
+}
