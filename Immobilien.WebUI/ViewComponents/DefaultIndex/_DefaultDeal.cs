ca6cf1a99ee4461dc5cf1c01fa2b5994ffcbe00c
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-07-22 19:54:46 +0200

    Prepeared Deal Section in UILayout

---
+++ Immobilien.WebUI/ViewComponents/DefaultIndex/_DefaultDeal.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Immobilien.Business.Abstract;
+using Immobilien.Dto.Dtos.DealDtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Immobilien.WebUI.ViewComponents.DefaultIndex
+{
+    public class _DefaultDeal : ViewComponent
+    {
+        private readonly IDealService _dealService;
+        private readonly IMapper _mapper;
+        public _DefaultDeal(IDealService dealService, IMapper mapper)
+        {
+            _dealService = dealService;
+            _mapper = mapper;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var datas = await _dealService.TGetListAsync();
+            var dealList = _mapper.Map<List<ResultDealDto>>(datas);
+            return View(dealList);
+        }
+    }
+}
