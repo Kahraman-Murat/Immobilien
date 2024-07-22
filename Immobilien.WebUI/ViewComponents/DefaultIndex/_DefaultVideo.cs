e6f4e0a570bae7060a346783d592fe3db3cb2e03
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-07-22 16:57:53 +0200

    Prepeared VideoController

---
+++ Immobilien.WebUI/ViewComponents/DefaultIndex/_DefaultVideo.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Immobilien.Business.Abstract;
+using Immobilien.Dto.Dtos.VideoDtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Immobilien.WebUI.ViewComponents.DefaultIndex
+{
+    public class _DefaultVideo : ViewComponent
+    {
+        private readonly IVideoService _videoService;
+        private readonly IMapper _mapper;
+
+        public _DefaultVideo(IMapper mapper, IVideoService videoService)
+        {
+            _mapper = mapper;
+            _videoService = videoService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var datas = await _videoService.TGetListAsync();
+            var videoList = _mapper.Map<List<ResultVideoDto>>(datas);
+            return View(videoList);
+        }
+    }
+}
+
