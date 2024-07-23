4e0f470f7efa232df69a1d3298d95c51f6c10d41
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-07-23 17:16:52 +0200

    Prepeared SubHeader Section in UILayout

---
+++ Immobilien.WebUI/ViewComponents/UILayout/_UISubHeader.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Immobilien.Business.Abstract;
+using Immobilien.Dto.Dtos.SubHeaderDtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Immobilien.WebUI.ViewComponents.UILayout
+{
+    public class _UISubHeader: ViewComponent
+    {
+        private readonly ISubHeaderService _subHeaderService;
+        private readonly IMapper _mapper;
+        public _UISubHeader(ISubHeaderService subHeaderService, IMapper mapper)
+        {
+            _subHeaderService = subHeaderService;
+            _mapper = mapper;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var datas = await _subHeaderService.TGetListAsync(); 
+            var subHeaderList = _mapper.Map<List<ResultSubHeaderDto>>(datas);
+            return View(subHeaderList);
+
+        }
+    }
+}
